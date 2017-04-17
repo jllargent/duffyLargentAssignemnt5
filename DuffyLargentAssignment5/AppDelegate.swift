b56066b5e095b483ab9013b9afd69430e32b896c
Jon Largent <jllargent@bsu.edu>
2017-04-17 09:30:05 -0400

    Initial Commit

---
+++ DuffyLargentAssignment5/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DuffyLargentAssignment5
+//
+//  Created by Jon Largent on 4/17/17.
+//  Copyright © 2017 largent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DuffyLargentAssignment5/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DuffyLargentAssignment5
+//
+//  Created by Jon Largent on 4/17/17.
+//  Copyright © 2017 largent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
