a491c2ce49ed075fabf352b3aec7b5a735a122ba
Jon Largent <jllargent@bsu.edu>
2017-04-24 01:22:36 -0400

    got all but 2 functions done. still need to do age and if they have certain social media

--- DuffyLargentAssignment5/ViewController.swift
+++ DuffyLargentAssignment5/ViewController.swift
@@ -10,10 +10,12 @@ import Cocoa
 
 class ViewController: NSViewController {
     
-    var queryTypeArray = ["Name","Username","Facebook Friend","Reddit Post","Twitter Following","Social Media","Age Range","Snapchat Story","Reddit Karma","Twitter Followers"]
+    var queryTypeArray = ["Name","Twitter Username","Facebook Friend","Reddit Post","Twitter Following","Social Media","Age Range","Snapchat Story","Reddit Karma","Twitter Followers"]
     var results = ""
     var query = ""
     var queryType = 0
+    let emptyText = NSMutableAttributedString(string: "")
+    let noData = NSMutableAttributedString(string: "No Data Exists")
     var nameDictionary = ["Trent Kaniuga":["Irvine, California","","English","TrentKaniuga","","","","trent_k_arts"],
         "Charlie Bowater":["United Kingdom","","English","charliebowater","","","","charliebowater"],
         "Matt Carriker":["Texas","","English","demolitionranch","","","","demolition_ranch"],
@@ -30,7 +32,48 @@ class ViewController: NSViewController {
         "Amy Rose Williams ":["Solihull, England ","15-APR-92","English","CaptainLuxCanis","Amii Rose","","","CaptainLuxCanis"],
         "Nicole Gustafsson":["Washington","","English","Nimasprout","","","","Nimasprout"]
     ]
+    var facebookDictionary = ["Julia Largent":["Julia Largent","1138","01-SEP-06"],
+        "Aidan Kearney":["Aidan Kearney","653",""],
+        "Jon Largent":["]jllargent@gmail.com","515","25-SEP-05"],
+        "Meghan Duffy":["Meghan Duffy","241","03-SEP-14"],
+        "Nick Dowell":["Nick Dowell","174",""],
+        "Carly Jensen":["Carly Jensen","1000","01-JAN-08"],
+        "Basil":["Smallherbfriend","17","14-AUG-14"],
+        "Jack McGinnis":["Jack T McGinnis","502","21-FEB-13"],
+        "Divya":["Divya Sarda","187",""],
+        "Amy Rose Williams":["Amii Rose","62","13-OCT-09"]
+    ]
+    var redditDictionary = ["Julia Largent":["Jellos4peace","","312","","06-DEC-10"],
+        "Jon Largent":["mushsogene","27","11","7","28-OCT-11"],
+        "Meghan Duffy":["smalls2233","1","132","22","31-AUG-14"],
+        "Nick Dowell":["IAmTheWeregoose","21","1104","30","05-MAY-14"]
 
+    ]
+    var twitterDictionary = ["Trent Kaniuga":["TrentKaniuga","238","2678","1066","01-JAN-10"],
+                             "Charlie Bowater":["charliebowater","337","20500","6697","01-JUN-09"],
+        "Matt Carriker":["demolitionranch","21","42400","373","01-APR-14"],
+        "Julia Largent":["jellos4peace","1197","521","15700","15-JUN-09"],
+        "Aidan Kearney":["kearnishmas","250","50","","01-JAN-15"],
+        "Jon Largent":["jon_largent","73","46","2646","15-MAR-08"],
+        "Meghan Duffy":["smalls2233","470","287","10500","15-JUN-09"],
+        "Nick Dowell":["ncdowell","215","184","4726","15-APR-11"],
+        "Carly Jensen":["carlywankenobi","345","390","23700","01-JAN-12"],
+        "Basil":["BasilGard3n","85","20","1098","01-JUN-15"],
+        "Matthew Yapp":["Mattyappish","360","182","2102",""],
+        "Jack McGinnis":["NativeAbearican","457","122","1720","25-DEC-15"],
+        "Divya":["legendofdivya","135","38","354","25-MAR-15"],
+        "Amy Rose Williams":["CaptainLuxCanis","232","516","","25-MAR-09"]
+
+    ]
+    var snapchatDictionary = ["Julia Largent":["Jellos4peace","","1"],
+        "Aidan Kearney":["kearnishmas","","1"],
+        "Jon Largent":["mushsogene","26","0"],
+        "Meghan Duffy":["smalls2233","55","1"],
+        "Nick Dowell":["ncdowell","25","1"],
+        "Carly Jensen":["sassyhairflip","","0"],
+        "Matthew Yapp":["Mattyappish","53","1"],
+        "Divya":["divi_s5","79","1"]
+    ]
     
     @IBOutlet var resultsTextViewer: NSTextView!
     @IBOutlet weak var resultsViewer: NSScrollView!
@@ -75,6 +118,7 @@ class ViewController: NSViewController {
 
     func nameChoice(query: String) {
         print("name choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
         for (name, info) in nameDictionary{
             results = "Name     Location        Brithdate       Language        Twitter Username    Facebook Username   Snapchat Username   Reddit Username Instagram Username \n"
             if query.caseInsensitiveCompare(name) == .orderedSame{
@@ -86,25 +130,89 @@ class ViewController: NSViewController {
                 print(results)
                 let printResults = NSMutableAttributedString(string: results)
                 resultsTextViewer.textStorage?.append(printResults)
-                
             }
+            
         }
     }
     
     func usernameChoice(query: String) {
         print("username choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in nameDictionary{
+            results = "Name     Location        Brithdate       Language        Twitter Username    Facebook Username   Snapchat Username   Reddit Username Instagram Username \n"
+            if query.caseInsensitiveCompare(info[3]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+          
+        }
+
     }
     
     func facebookFriendChoice(query: String) {
         print("facebook friend choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in facebookDictionary{
+            results = "Name     Username        Friends Count       Date Joined\n"
+            if query.caseInsensitiveCompare(info[1]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
     
     func redditPostChoice(query: String) {
         print("reddit post choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in redditDictionary{
+            results = "Name     Username    Subreddits      Karma       Post Count      Date Joined\n"
+            if query.caseInsensitiveCompare(info[3]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
     
     func twitterFollowingChoice(query: String) {
         print("twitter following choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in twitterDictionary{
+            results = "Name     Username    Following      Followers       Post Count      Date Joined\n"
+            if query.caseInsensitiveCompare(info[1]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
     
     func socialMediaChoice(query: String) {
@@ -117,14 +225,68 @@ class ViewController: NSViewController {
     
     func snapchatStoryChoice(query: String) {
         print("snapchat story choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        var snapQuery = ""
+        if query.caseInsensitiveCompare("yes") == .orderedSame{
+            snapQuery = "1"
+        }else{
+            snapQuery = "0"
+        }
+        for (name, info) in snapchatDictionary{
+            results = "Name     Username    Friends      Story\n"
+            if snapQuery.caseInsensitiveCompare(info[2]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
     
     func redditKarmaChoice(query: String) {
         print("reddit karma choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in redditDictionary{
+            results = "Name     Username    Subreddits      Karma       Post Count      Date Joined\n"
+            if query.caseInsensitiveCompare(info[2]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
     
     func twitterFollowersChoice(query: String) {
         print("twitter followers choice " + query)
+        resultsTextViewer.textStorage?.setAttributedString(emptyText)
+        for (name, info) in twitterDictionary{
+            results = "Name     Username    Following      Followers       Post Count      Date Joined\n"
+            if query.caseInsensitiveCompare(info[2]) == .orderedSame{
+                results.append(name + "    ")
+                for data in info{
+                    results.append(data + "   ")
+                }
+                results.append("\n")
+                print(results)
+                let printResults = NSMutableAttributedString(string: results)
+                resultsTextViewer.textStorage?.append(printResults)
+            }
+            
+        }
+
     }
 }
 
